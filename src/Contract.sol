b53ae79bfb799e5443679f5e67a685ae66a2db47
vrunjm <37998755+vrunjm@users.noreply.github.com>
2026-03-06 22:52:04 +0800

    update

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,10 +1,30 @@
-// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
 contract Contract {
     uint public counter;
 
+    function MyTrasfer(address payable to ) public view returns (uint){
+        if to.balance>10 && address(this).balance<=10{
+            x.transfer(1 wei);
+        }
+    }
+
+
+
+    constructor() public payable{
+        counter = 0;
+    }
+
     function add() public {
         counter += 1;
     }
+
+    function subtract() public {
+        counter -= 1;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
 }
