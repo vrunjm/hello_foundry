56eb4af4ab63951def92ece6c040f4e23504ace9
vrunjm <37998755+vrunjm@users.noreply.github.com>
2026-03-06 22:02:12 +0800

    test

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract Contract {
+    uint public counter;
+
+    function add() public {
+        counter += 1;
+    }
+}
